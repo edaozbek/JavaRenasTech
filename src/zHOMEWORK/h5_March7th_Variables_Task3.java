583621c0d04b62ccc3f2cf16691a683aa4d8d756
edaozbek <edaozbek@gmail.com>
2022-04-13 23:21:35 -0400

    first commit

---
+++ src/zHOMEWORK/h5_March7th_Variables_Task3.java
@@ -0,0 +1,23 @@
+package zHOMEWORK;
+
+public class h5_March7th_Variables_Task3 {
+    // check if 350 is divisible by 2 , 3 , 5 (Check for each number)
+    // if it is divisible print : 350 is divisible by 2 true
+    // if it is not divisible print : 350 is divisible by 2 false public static void main(String[] args) {
+public static void main (String[] args){
+
+        int n1 = 350;
+        byte n2 = 2;
+        byte n3 = 3;
+        byte n4 = 5;
+
+        boolean bo1 = 350 != n2; // true
+        boolean bo2 = 350 == n3; // false
+        boolean bo3 = 350 != n4; // true
+        boolean bo4 = 50 == -7; // false
+
+        System.out.println(n1 + "is divisible by 2 :" + bo1);
+        System.out.println(n1 + "is divisible bye 3 :" + bo2);
+        System.out.println(n1 + "is divisible bye 5 : " + bo3);
+        System.out.println(n1 + "is divisible bye -7 : " + bo4);
+    }}
---
+++ src/zHOMEWORK/h6_March9_12Tasks.java
@@ -0,0 +1,174 @@
+package zHOMEWORK;
+
+public class h6_March9_12Tasks {
+
+    //Task 1. Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+//Expected Output :
+//Hello
+//Alexandra Abramov
+
+
+    //System.out.println("Hello");
+    //System.out.println("Eda Dede");
+
+
+    //  }
+
+    //2. Write a Java program to print the sum of two numbers.
+//Test Data:
+//74 + 36
+//Expected Output :
+//110
+
+
+    //byte a1=74;
+    //byte a2=36;
+
+    //  System.out.println(a1+a2);
+
+
+    //3. Write a Java program to divide two numbers and print on the screen.
+//Test Data :
+//50/3
+//Expected Output :
+//16
+
+    //public static void main(String[] args) {
+
+       // int n1 = 50;
+       // int n2 = 3;
+       // System.out.println(n1 / n2);
+
+
+    //4. Write a Java program to print the result of the following operations.
+//Test Data:
+//a. -5 + 8 * 6
+//b. (55+9) % 9
+//c. 20 + -3*5 / 8
+//d. 5 + 15 / 3 * 2 - 8 % 3
+//Expected Output :
+//43
+//1
+//19
+//13
+
+    //public static void main (String[] args) {
+      //  System.out.println( -5 + 8 * 6);
+      //  System.out.println((55+9) % 9);
+      //  System.out.println(20 + -3*5 / 8);
+      //  System.out.println(5 + 15 / 3 * 2 - 8 % 3); }}
+
+    //5. Write a Java program that takes two numbers as input and display the product of two numbers.
+//Test Data:
+//Input first number: 25
+//Input second number: 5
+//Expected Output :
+//25 x 5 = 125
+
+    ///public static void main (String [] args){
+
+   // byte n1=25;
+   // byte n2=5;
+   // System.out.println((n1+"x"+n2)+"="+(n1*n2));}}
+
+
+    //6. Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
+//Test Data:
+//Input first number: 125
+//Input second number: 24
+//Expected Output :
+//125 + 24 = 149
+//125 - 24 = 101
+//125 x 24 = 3000
+//125 / 24 = 5
+
+   // public static void main(String[] args) {
+
+    //   int n1=125;
+    //    int n2=24;
+    //    System.out.println ((n1+"+"+n2)+"="+ (n1+n2));
+    //    System.out.println((n1+"-"+n2)+"="+ (n1-n2));
+    //    System.out.println((n1+"x"+n2)+"="+ (n1*n2));
+    //    System.out.println((n1+"/"+n2)+"="+ (n1/n2));
+
+
+
+    //7. Write a Java program that takes a number as input and prints its multiplication table upto 10.
+//Test Data:
+//Input a number: 8
+//Expected Output :
+//8 x 1 = 8
+//8 x 2 = 16
+//8 x 3 = 24
+//...
+//8 x 10 = 80
+
+   // public static void main(String[] args) {
+
+     //   int n=8;
+     //   System.out.println((n+"x1")+"="+(n*1));
+     //   System.out.println((n+"x2")+"="+(n*2));
+     //   System.out.println((n+"x3")+"="+(n*3));
+     //   System.out.println((n+"x4")+"="+(n*4));
+     //   System.out.println((n+"x5")+"="+(n*5));
+     //   System.out.println((n+"x6")+"="+(n*6));
+     //   System.out.println((n+"x7")+"="+(n*7));
+     //   System.out.println((n+"x8")+"="+(n*8));
+     //   System.out.println((n+"x9")+"="+(n*9));
+     //   System.out.println((n+"x10")+"="+(n*10));
+
+
+
+
+    //8. Write a Java program to display the following pattern.
+//Sample Pattern :
+//
+//  J  a  v   v a
+//  J  a a  v  v a a
+//J J aaaaa  V V aaaaa
+// JJ a   a  V a   a
+    /*public static void main(String[] args) {
+        System.out.println("J  a  v   v a");
+        System.out.println("J  a a  v  v a a");
+        System.out.println("J J aaaaa  V V aaaaa");
+        System.out.println("JJ a   a  V a   a");
+
+    }}*/
+    //9. Write a Java program to compute the specified expressions and print the output.
+//Test Data:
+//((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+//Expected Output
+//2.138888888888889
+
+   /* public static void main(String[] args) {
+
+
+        System.out.println((25.5*3.5-3.5*3.5)/(40.5-4.5));
+
+
+    }
+}*/
+    //10. Write a Java program to compute a specified formula.
+//Specified Formula :
+//4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+//Expected Output
+//2.9760461760461765
+
+   /* public static void main(String[] args) {
+
+
+        System.out.println((4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))));
+    }}*/
+    //12. Write a Java program that takes three numbers as input to calculate and print the average of the numbers.
+   public static void main(String[] args) {
+
+       int n1=20;
+       int n2=30;
+       int n3=40;
+       int totaln=n1+n2+n3;
+       int av=3;
+       System.out.println(totaln/av);
+  }
+}
+
+
